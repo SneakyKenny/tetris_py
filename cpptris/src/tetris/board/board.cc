f54f4877517c52e1ca9989b00bbe9b5dcbad20ec
Kenny Lorin <kenny.lorin@epita.fr>
2021-06-14 14:45:22 +0200

    add board

---
+++ cpptris/src/tetris/board/board.cc
@@ -0,0 +1,301 @@
+#include "tetris/board/board.hh"
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <random>
+
+#include "utype.hh"
+
+namespace tetris::board
+{
+    Board::Board()
+        : active_piece_position_(0, 0) /* FIXME: this is ugly */
+    {
+        ensure_complete_queue(true);
+        spawn_next_piece();
+    }
+
+    void Board::ensure_complete_queue(bool init)
+    {
+        static std::random_device rd;
+        static std::mt19937 g(rd());
+
+        if (init)
+        {
+            for (size_t i = 0; i < PIECE_QUEUE_BAG_SIZE; i++)
+                second_bag_.push_back(static_cast<piece::PieceType>(i));
+
+            std::shuffle(second_bag_.begin(), second_bag_.end(), g);
+        }
+
+        if (queue_.size() == 0)
+        {
+            // Swap empty current queue with full secondary bag
+            std::swap(queue_, second_bag_);
+
+            // fill secondary bag
+            for (size_t i = 0; i < PIECE_QUEUE_BAG_SIZE; i++)
+                second_bag_.push_back(static_cast<piece::PieceType>(i));
+
+            // shuffle second bag
+            std::shuffle(second_bag_.begin(), second_bag_.end(), g);
+        }
+
+        /*
+        std::cout << "Queue after ensure_complete_queue:" << std::endl;
+        for (auto const& p : queue_)
+            std::cout << utils::utype(p) << " ";
+
+        std::cout << std::endl << "Secondary queue:" << std::endl;
+        for (auto const& p : second_bag_)
+            std::cout << utils::utype(p) << " ";
+
+        std::cout << std::endl;
+        */
+    }
+
+    bool Board::get_cell(size_t x, size_t y) const
+    {
+        assert(x < BOARD_WIDTH);
+        assert(y < BOARD_HEIGHT * 2);
+        return board_.test(xy_to_i(x, y));
+    }
+
+    void Board::set_cell(size_t x, size_t y, bool val)
+    {
+        assert(x < BOARD_WIDTH);
+        assert(y < BOARD_HEIGHT * 2);
+        board_.set(xy_to_i(x, y), val);
+    }
+
+    bool Board::spawn_next_piece()
+    {
+        ensure_complete_queue();
+
+        piece::PieceType piece_type = queue_.front();
+        queue_.pop_front();
+
+        /*
+        std::cout << utils::utype(piece_type) << std::endl
+            << "This piece will spawn at "
+            << piece::PieceHelper::get_piece_spawn_position(piece_type)
+            << std::endl;
+        */
+
+        piece::PiecePosition spawn_position =
+            piece::PieceHelper::get_piece_spawn_position(piece_type);
+
+        // spawn_position = piece::PiecePosition(-1, 20, piece::PieceRotation::R_N);
+
+        if (!put_piece_at(piece_type, spawn_position))
+            return false;
+
+        active_piece_type_ = piece_type;
+        active_piece_position_ = spawn_position;
+
+        return true;
+    }
+
+    void Board::disable_current_piece()
+    {
+        piece::piece_matrix_t piece_matrix = piece::PieceMatrix::get_piece_matrix(
+                active_piece_type_, active_piece_position_.get_rotation());
+
+        size_t piece_size = piece::PieceMatrix::get_piece_size(active_piece_type_);
+
+        for (size_t y = 0; y < piece_size; y++)
+        {
+            for (size_t x = 0; x < piece_size; x++)
+            {
+                size_t inpiece_index = piece::PieceMatrix::
+                    get_inpiece_index(x, y, active_piece_type_);
+
+                if (!piece_matrix.test(inpiece_index))
+                    continue;
+
+                size_t index = xy_to_i(x + active_piece_position_.get_x(),
+                        y + active_piece_position_.get_y());
+
+                board_.set(index, false);
+            }
+        }
+    }
+
+    bool Board::is_valid_move(char dx, char dy, char dr) const
+    {
+        // Check only one of dx, dy, or dr is set. Also,
+        // check dx, dy is one of [-1, 1], dr in [-2, -1, 1, 2] as well
+
+        if ((dx && (dy || dr))
+                || (dy && (dx || dr))
+                || (dr && (dx || dy)))
+        {
+            std::cerr << "The fuck are you doing ?" << std::endl;
+            return false;
+        }
+
+        if (dr)
+            return std::abs(dr) <= 2;
+
+        return std::abs(dx ? dx : dy) == 1;
+    }
+
+    bool Board::rotate_piece(char dr)
+    {
+        piece::PieceRotation new_rotation = active_piece_position_.get_rotation() + dr;
+        /*
+        std::cout << "rotation is now: " << new_rotation << std::endl;
+        std::cout << "matrix is: "
+            << piece::PieceMatrix::get_piece_matrix(active_piece_type_, new_rotation)
+            << std::endl
+            << piece::PieceHelper::piece_to_string(active_piece_type_, new_rotation)
+            << std::endl;
+        */
+
+        disable_current_piece();
+
+        // - Get kick table corresponding to the active piece
+        piece::kick_table_t kick_table = piece::get_kick_table(
+                active_piece_type_, active_piece_position_.get_rotation(), new_rotation);
+
+        // - Test each kick offset
+        for (auto const& kick : kick_table)
+        {
+            /*
+            std::cout << "trying kick: "
+                    << kick.first << ", " << kick.second
+                    << std::endl;
+            */
+
+            piece::PiecePosition new_position{
+                active_piece_position_.get_x() + kick.first,
+                active_piece_position_.get_y() + kick.second,
+                new_rotation
+            };
+
+            if (put_piece_at(active_piece_type_, new_position))
+            {
+                /*
+                std::cout << "rotation succeeded for kick: "
+                    << kick.first << ", " << kick.second
+                    << std::endl;
+                */
+
+                active_piece_position_ = new_position;
+                return true;
+            }
+
+            /*
+            std::cout << "rotation failed for kick: "
+                    << kick.first << ", " << kick.second
+                    << std::endl;
+            */
+        }
+
+        // Restore active piece
+        put_piece_at(active_piece_type_, active_piece_position_);
+        return false;
+    }
+
+    bool Board::move_piece(char dx, char dy, char dr)
+    {
+        if (!is_valid_move(dx, dy, dr))
+            return false;
+
+        if (dr)
+            return rotate_piece(dr);
+
+        disable_current_piece();
+
+        piece::PiecePosition new_position = piece::PiecePosition(
+            active_piece_position_.get_x() + dx,
+            active_piece_position_.get_y() + dy,
+            active_piece_position_.get_rotation()
+        );
+
+        if (!put_piece_at(active_piece_type_, new_position))
+        {
+            put_piece_at(active_piece_type_, active_piece_position_);
+            return false;
+        }
+
+        active_piece_position_ = new_position;
+
+        return true;
+    }
+
+    bool Board::put_piece_at(piece::PieceType type, piece::PiecePosition position)
+    {
+        piece::piece_matrix_t piece_matrix =
+            piece::PieceMatrix::get_piece_matrix(type, position.get_rotation());
+
+        // std::cout << piece::PieceHelper::piece_to_string(type, position.get_rotation()) << std::endl;
+
+        size_t piece_size = piece::PieceMatrix::get_piece_size(type);
+
+        board_t save = board_;
+
+        for (size_t y = 0; y < piece_size; y++)
+        {
+            for (size_t x = 0; x < piece_size; x++)
+            {
+                size_t inpiece_index = piece::PieceMatrix::get_inpiece_index(x, y, type);
+
+                bool cell_state = piece_matrix.test(inpiece_index);
+
+                if (!cell_state)
+                    continue;
+
+                if (x + position.get_x() >= BOARD_WIDTH
+                        || y + position.get_y() >= BOARD_HEIGHT * 2)
+                {
+                    board_ = save;
+                    return false;
+                }
+
+                size_t index = xy_to_i(x + position.get_x(), y + position.get_y());
+
+                // TODO: Check we're not out of bounds
+                if (board_.test(index) && cell_state)
+                {
+                    board_ = save;
+                    return false;
+                }
+
+                board_.set(index, cell_state);
+            }
+        }
+
+        return true;
+    }
+
+    size_t Board::xy_to_i(size_t x, size_t y) const
+    {
+        return y * BOARD_WIDTH + x;
+    }
+
+    std::ostream& operator<<(std::ostream& o, const Board& b)
+    {
+        // TODO: Draw margin ?
+        for (ssize_t y = BOARD_HEIGHT + 2; y >= 0; y--)
+        {
+            o << piece::PieceRepresentation::get_border();
+
+            for (size_t x = 0; x < BOARD_WIDTH; x++)
+            {
+                o << (b.get_cell(x, y)
+                    ? piece::PieceRepresentation::get_taken()
+                    : piece::PieceRepresentation::get_empty());
+            }
+
+            o << piece::PieceRepresentation::get_border()
+                << std::endl;
+        }
+
+        for (ssize_t x = -1; x <= BOARD_WIDTH; x++)
+            o << piece::PieceRepresentation::get_border();
+
+        return o;
+    }
+}
